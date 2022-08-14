80f6b64facffa99022842d3dc0ff8d607e4a3398
“Beatriz <beatriz.hernandez27sp@gmail.com>
2022-08-13 20:30:06 -0500

    UPDATE -Se actualiza consumo de servicios

--- src/screens/MainDashboardProduct/index.js
+++ src/screens/MainDashboardProduct/index.js
@@ -1,40 +1,85 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { k } from '../../utils';
-import {GET_PRODUCT_BY_ID} from "../../api";
+import { GET_PRODUCT_BY_ID ,GET_PRODUCT_SEARCH} from "../../api";
+import { getMessageError } from '../../api/config';
 
 const MainDashboardProduct = () => {
+
 	const navigate = useNavigate();
-	const goToDetails = () => {
-		navigate(`${k.navigations.detailsProduct}1`);
+	const [errorMessage, setErrorMessage] = useState('');
+	const [arrayListProduct, setArrayListProduct] = useState([]);
+	//const [isLoader, setIsLoader] = useState(true);
+
+	const getProductSearch = async (text) =>{
+		await GET_PRODUCT_SEARCH(
+			text,
+			onSuccessGetProductSearch,
+			onErrorGetProductSearch,
+			onDoneGetProductSearch
+		);
 	};
-
-	useEffect(()=>{
-
-	});
-
-	const getProductSearch = async (id) =>{
-		await GET_PRODUCT_BY_ID(id, onSuccessGetProductSearch, onErrorGetProductSearch, onDoneGetProductSearch )
-	}
 
 	const onSuccessGetProductSearch = response =>{
 		console.log(response)
+		const { data, success } = response.data;
+		if (success) {
+			if(data){
+				const {items} = data;
+				setArrayListProduct(items);
+			}
+		}
 	};
 
 	const onErrorGetProductSearch = error =>{
 		console.log(error)
+		if (error && error.response && error.response.data) {
+			const {
+				response: {
+					data: { message }
+				}
+			} = error;
+			showError(message || getMessageError(error));
+		} else {
+			showError(getMessageError(error));
+		}
 	};
 
 	const onDoneGetProductSearch = () =>{
-
+		setErrorMessage(null);
 	}
 
+	const showError = message => {
+		setErrorMessage(message);
+	};
+
+	const goDetailsProduct = (id) => {
+		navigate(`${k.navigations.detailsProduct}${id}`);
+	}
 
 	return (
 		<div className='screens_root'>
-			<h1>Hola mundo MainDashboardProduct </h1>
-			<button onClick={() => goToDetails()}>detalle</button>
-			<p>hola mundo</p>
+			{
+				errorMessage &&  (
+					<h1>{errorMessage}</h1>
+				)
+			}
+			{arrayListProduct && arrayListProduct.length > 0 ? (
+				<>
+					{
+						arrayListProduct.map((item)=>(
+							<div key={item.id}>
+								<h1>{item.title}</h1>
+								<p>hola mundo</p>
+								<button onClick={() => goDetailsProduct(item.id)}>ver detalle de prodcuto</button>
+
+							</div>
+						))
+					}
+				</>
+				):(
+					<h1>No data</h1>
+				)}
 		</div>
 	);
 };
--- src/screens/MainDetailsProduct/index.js
+++ src/screens/MainDetailsProduct/index.js
@@ -1,16 +1,85 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, {useState,useEffect} from 'react';
+import { useNavigate , useParams} from 'react-router-dom';
+import { getMessageError } from '../../api/config';
+import {GET_PRODUCT_BY_ID} from '../../api'
+
 
 const MainDetailsProduct = () => {
+	/**HOOKS GENERALES*/
 	const navigate = useNavigate();
-	const goToDetails = () => {
+	const params = useParams();
+
+	/**HOOKS VARIABLES*/
+	const [errorMessage, setErrorMessage] = useState('');
+	const [loader, setIsLoader] = useState(false);
+	const [product, setProduct] = useState(null);
+
+	const goBack = () => {
 		navigate(-1);
+	};
+
+	useEffect(()=>{
+		if (params && params.code) {
+			setIsLoader(true);
+			const { id } = params;
+			getDetailsProduct(id)
+		}
+	}, [])
+
+
+	const getDetailsProduct  = (id) => {
+		GET_PRODUCT_BY_ID(
+			id,
+			onSuccessGetDetailsProduct,
+			onErrorGetDetailsProduct,
+			onDoneGetDetailsProduct
+		)
+	};
+
+	const onSuccessGetDetailsProduct = response =>{
+		console.log(response)
+		const { data, success } = response.data;
+		if (success) {
+			if(data){
+				const {item} = data;
+				setProduct(item);
+			}
+		}
+	};
+
+	const onErrorGetDetailsProduct = error =>{
+		console.log(error)
+		if (error && error.response && error.response.data) {
+			const {
+				response: {
+					data: { message }
+				}
+			} = error;
+			showError(message || getMessageError(error));
+		} else {
+			showError(getMessageError(error));
+		}
+	};
+
+	const onDoneGetDetailsProduct = () =>{
+		setErrorMessage(null);
+	}
+
+	const showError = message => {
+		setErrorMessage(message);
 	};
 
 	return (
 		<>
-			<h1>Hola mundo MainDetailsProduct </h1>
-			<button onClick={() => goToDetails()}>Regresar</button>
+			{
+				errorMessage &&  (
+					<h1>{errorMessage}</h1>
+				)
+			}
+			<div>
+				<h1>{product.title} </h1>
+				<button onClick={() => goBack()}>Regresar</button>
+			</div>
 		</>
 	);
 };
